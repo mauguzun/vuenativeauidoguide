6019ded3adaed9076a83835b95c7b5f7ef45e302
svc_lifa4@aslairlines.com <svc_lifa4@aslairlines.com>
2019-03-12 12:29:29 +0200

    50 m in settings

--- app/components/locationSettings.js
+++ app/components/locationSettings.js
@@ -1,9 +1,9 @@
 export const locationSettings = {
-  minimumUpdateTime: 5000,
-  updateTime: 5000,
+  minimumUpdateTime: 5000,//  5 sec
+  updateTime: 5000, //  5 sec
   updateDistanceInMetters: 0.1,
-  samePlaceInKm: 0.01,
-  pointCanPlaceDistanceKm: 0.07,
+  samePlaceInKm: 0.001, // 10 me
+  pointCanPlaceDistanceKm: 0.05, // 50 metter
   featurePointDistanceKm: 0.6,
   maximumAge: 50000, 
   color: {
